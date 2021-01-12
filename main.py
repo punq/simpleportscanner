bf797abcaac842ecbef8b32d3a9a413f6ff26997
punq <diego.a.flores@hotmail.com>
2021-01-11 20:53:01 -0700

    Made the program more advanced

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import socket
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(0.5)
 
-host = "192.168.1.85"
-port = 135
+host = input("What is the IP address you'd like to scan?: ")
+# port = int(input("What is the port you'd like to scan?: "))
 
 def portscanner(port):
-    if soc.connect_ex((host, port)):
+    if soc.connect_ex((host,port)):
         print("Port " + str(port) + " is closed")
     else:
-        print("Port " + str(port) + " is opened")
+        print("Port " + str(port) + " is open")
 
-portscanner(port)
-
+for port in range(1,1000):
+    portscanner(port)
